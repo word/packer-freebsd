02ba8772cbd201e274879b38a7f7aa8510ea4edf
Andrew Wasilczuk <akw@ml1.net>
2015-09-09 23:41:57 +0100

    Removed stuff conflicting with salt vagrantfile

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -17,7 +17,6 @@ if [ "$PACKER_BUILDER_TYPE" = 'vmware-iso' ]; then
 	echo 'vmware_guestd_enable="YES"' >> /etc/rc.conf
 elif [ "$PACKER_BUILDER_TYPE" = 'virtualbox-iso' ]; then
 	pkg install -y virtualbox-ose-additions
-	echo 'ifconfig_em1="inet 10.6.66.42 netmask 255.255.255.0"' >> /etc/rc.conf
 	echo 'vboxguest_enable="YES"' >> /etc/rc.conf
 	echo 'vboxservice_enable="YES"' >> /etc/rc.conf
 else
